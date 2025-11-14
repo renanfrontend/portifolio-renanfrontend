76016b61f5f21f860479844e81e0527ee8965828
renanfrontend <146970438+renanfrontend@users.noreply.github.com>
2025-11-14 01:04:20 +0000

    refactor: update deployment workflow and refine Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: [
     './index.html',
-    './**/*.{js,ts,jsx,tsx,html}',
+    './main.tsx',
+    './index.tsx',
+    './App.tsx',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './hooks/**/*.{js,ts,jsx,tsx}',
+    './services/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
